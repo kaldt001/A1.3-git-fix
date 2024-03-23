3bf7379e3dfe97b603e769878c466a00c0cde7da
kaldt001 <kaldt001@mymail.unisa.edu.au>
2024-03-23 17:37:31 +1030

    Update .gitignore
    Created a basic constructor for the "Item" class. Item class was also turned into an abstract class as it shouldn't be instatiated.

--- src/students/items/Item.java
+++ src/students/items/Item.java
@@ -1,6 +1,18 @@
 package students.items;
 
-public class Item {
-
+public abstract class Item {
+	private int age;
+	private int maturationAge;
+	private int deathAge;
+	private double monetaryValue;
+	
+	public Item(int age, int maturationAge, int deathAge, double monetaryValue) {
+		this.age = age;
+		this.maturationAge = maturationAge;
+		this.deathAge = deathAge;
+		this.monetaryValue = monetaryValue;
+		
+	}
 	
 }
+	
