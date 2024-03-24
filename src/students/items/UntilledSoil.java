a5f0cfb5ea1bdf6e292bf100988f33b8e50c3a89
kaldt001 <kaldt001@mymail.unisa.edu.au>
2024-03-25 10:07:36 +1030

    Modified untilled soil class
    Added code essentially identical to the weed class to the untilled soil class.

--- src/students/items/UntilledSoil.java
+++ src/students/items/UntilledSoil.java
@@ -1,6 +1,19 @@
 package students.items;
 
-public class UntilledSoil {
+public class UntilledSoil extends Item {
 
+    public UntilledSoil() {
+        super(Integer.MAX_VALUE, Integer.MAX_VALUE, -1, -1.0);
+    }
 
+    @Override
+    public boolean died() {
+        return false;
+        // Because untilled soil can also never die.
+    }
+
+    @Override
+    public String toString() {
+        return "/";
+    }
 }
