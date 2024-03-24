de48f9ee6f408d45f91aaf96b59582e7e6267013
kaldt001 <kaldt001@mymail.unisa.edu.au>
2024-03-24 14:28:33 +1030

    abstract Food item subclass
    Overrode toString method for the food class, which inherits it from the item class. Created a simple constructor, referencing the item super class and changed the food class into an abstract class.

--- src/students/items/Food.java
+++ src/students/items/Food.java
@@ -1,6 +1,17 @@
 package students.items;
 
-public class Food {
+public abstract class Food extends Item{
+	
+	public Food(int age, int maturationAge, int deathAge, double monetaryValue) {
+        super(age, maturationAge, deathAge, monetaryValue);
+    }
+	
+	@Override
+	public String toString() {
+        // Implement the string representation for Food
+        return "Food: \n age= " + getAge() + ", maturationAge= " + getMaturationAge() + ", deathAge= " + getDeathAge() + ", monetaryValue= " + getMonetaryValue();
+    }
+	
 
 
 }
