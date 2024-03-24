a12de51530ea873112ab683d0dbd5c12f79520dc
kaldt001 <kaldt001@mymail.unisa.edu.au>
2024-03-24 19:10:03 +1030

    Grain Subclass Creation, Constructor, Grain increment + getter
    Specified the Grain class as a subclass of the "Food" super class. Aptly, created a constructor for it, proceeded by a method that is purposed for managing the count of grain objects instantiated and a getter for that method's most crucial, associated variable.

--- src/students/items/Grain.java
+++ src/students/items/Grain.java
@@ -1,6 +1,24 @@
 package students.items;
+public class Grain extends Food {
+    private static int numOfGrains = 0; // This is just a variable the total number of grain objects that have been created
+    private static final double COST_OF_GRAIN = 1.0; 
 
-public class Grain{
+    
+    public Grain(int age, int maturationAge, int deathAge, double monetaryValue) {
+        super(age, maturationAge, deathAge, monetaryValue);
+        incrementNumOfGrains(); // This increments the "numOfGrains" variable every time the constructor is used (at every instantiation).
+    }
 
-	
+    // This method adds 1 to the numOfGrains object, used for increasing the count on the number of grains as seen above.
+    private static void incrementNumOfGrains() {
+        numOfGrains++;
+    }
+
+
+    public static int getNumOfGrains() {
+        return numOfGrains;
+    }
+
+
+
 }
