526e15d15719ed66a8429eb66ec93b0d8522f9d8
kaldt001 <kaldt001@mymail.unisa.edu.au>
2024-03-25 10:11:45 +1030

    Finished soil class
    Added another set of code, basically identical to the weed code once again, but this time for the soil class.

--- src/students/items/Soil.java
+++ src/students/items/Soil.java
@@ -1,5 +1,19 @@
 package students.items;
 
-public class Soil {
+public class Soil extends Item {
 
+    public Soil() {
+        super(Integer.MAX_VALUE, Integer.MAX_VALUE, 0, 0.0);
+    }
+
+    @Override
+    public boolean died() {
+        return false;
+     // Because like weed and untilled soil, soil can't die.
+    }
+
+    @Override
+    public String toString() {
+        return ".";
+    }
 }
