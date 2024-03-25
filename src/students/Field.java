b6a841dbe037bc57291ffeb53523a641807a6358
kaldt001 <kaldt001@mymail.unisa.edu.au>
2024-03-25 22:32:17 +1030

    completed field constructor alongsode the creation of the intializeField method method
    Took the laid out blue print to create a constructor for the field class. Then, created a nested loop to inititalize the field, in a method called "initialize field"

--- src/students/Field.java
+++ src/students/Field.java
@@ -1,10 +1,20 @@
 package students;
 
+import students.items.*;
 
 public class Field {
-	
-	public Field(int height, int width)
-	{
-	}
-	
+    private Item[][] field;
+
+    public Field(int height, int width) {
+        field = new Item[height][width];
+        initializeField();
+    }
+
+    private void initializeField() {
+        for (int row = 0; row < field.length; row++) {
+            for (int col = 0; col < field[0].length; col++) {
+                field[row][col] = new Soil();
+            }
+        }
+    }
 }
