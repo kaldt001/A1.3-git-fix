3f7d4bc72b6fc562565a894ff3af943819734854
kaldt001 <kaldt001@mymail.unisa.edu.au>
2024-03-27 09:49:29 +1030

    tick method
    created tick method, currently depends on a current age value, may be modified depending on its behaviour.

--- src/students/Field.java
+++ src/students/Field.java
@@ -2,11 +2,15 @@ package students;
 
 import students.items.*;
 
+import java.util.Random;
+
 public class Field {
     private Item[][] field;
+    private Random random;
 
     public Field(int height, int width) {
         field = new Item[height][width];
+        random = new Random();
         initializeField();
     }
 
@@ -14,6 +18,16 @@ public class Field {
         for (int row = 0; row < field.length; row++) {
             for (int col = 0; col < field[0].length; col++) {
                 field[row][col] = new Soil();
+            }
+        }
+    }
+
+    public void tick() {
+        for (int row = 0; row < field.length; row++) {
+            for (int col = 0; col < field[0].length; col++) {
+                Item currentItem = field[row][col];
+                int currentAge = currentItem.getAge(); // Getting the storing current age value in an int variable because the following tick() method needs an integer as an argument
+                currentItem.tick(currentAge); // Uses the tick method on the current item, based on its current age value.
             }
         }
     }
