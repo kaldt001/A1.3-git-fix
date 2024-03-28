42ac33e40e4dba62ce986516cc40ab7f9e689f92
kaldt001 <kaldt001@mymail.unisa.edu.au>
2024-03-28 17:33:17 +1030

    Farm runMethod (input)
    Started creating the run method, providing users with all the specified input options and a scanner to read said input.

--- src/students/Farm.java
+++ src/students/Farm.java
@@ -1,5 +1,7 @@
 package students;
 
+import java.util.Scanner;
+
 public class Farm {
 	private Field field;
 	    private double startingFunds;
@@ -9,8 +11,23 @@ public class Farm {
 	        this.startingFunds = startingFunds;
 	    }
 	
-	public void run()
-	{
-	}
+	    public void run() {
+	        Scanner scanner = new Scanner(System.in);
+	        boolean gameIsRunning = true;
+
+	        while (gameIsRunning) {
+	            System.out.println(field.toString());
+	            System.out.println("Bank balance: $" + startingFunds);
+	            System.out.println("Enter your next action:");
+	            System.out.println("  t x y: till");
+	            System.out.println("  h x y: harvest");
+	            System.out.println("  p x y: plant");
+	            System.out.println("  s: field summary");
+	            System.out.println("  w: wait");
+	            System.out.println("  q: quit");
+
+	            String playerInput = scanner.nextLine();
+	        }
+	    }
 	
 }
