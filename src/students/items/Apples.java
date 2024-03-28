818bf2bfbc48ab77799f259f6ee659f585a4715f
kaldt001 <kaldt001@mymail.unisa.edu.au>
2024-03-28 15:18:32 +1030

    Accidentally omitted Apple class code?
    I may have deleted code from my Apple class without even realising it. Promptly resolving this issue right now!

--- src/students/items/Apples.java
+++ src/students/items/Apples.java
@@ -1,5 +1,22 @@
 package students.items;
 
-public class Apples {
+public class Apples extends Food {
+    private static int generationCount = 0;
+    private static final double APPLE_COST = 2.0;
 
+    public Apples(int age, double monetaryValue) {
+        super(age, 3, 5, monetaryValue);
+        incrementGenerationCount();
+    }
+
+    private static void incrementGenerationCount() {
+        generationCount++;
+    }
+
+    public static int getGenerationCount() {
+        return generationCount;
+    }
+
+    
 }
+
