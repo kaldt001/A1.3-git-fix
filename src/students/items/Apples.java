ac4647ad41124bf568c4e97b938167aa02db3008
kaldt001 <kaldt001@mymail.unisa.edu.au>
2024-03-28 15:20:28 +1030

    Apples toString/Getter and field GetSumamry
    completed the Apple class implementation, and suddenly my field's getSummary isn't upset anymore!

--- src/students/items/Apples.java
+++ src/students/items/Apples.java
@@ -16,6 +16,20 @@ public class Apples extends Food {
     public static int getGenerationCount() {
         return generationCount;
     }
+    
+ // The Apples (not the company!) class' version of the toString method 
+    public String toString() {
+        if (getAge() < getMaturationAge()) {
+            return "a";
+        } else {
+            return "A";
+        }
+    }
+
+    // Gets the monetary value of an Apple.
+    public static double getAppleCost() {
+        return APPLE_COST;
+    }
 
     
 }
